ea9044fde7f1ab66e9d0b09c1c23466b327b7d3c
PolivodaMaxx <polivoda-maksim@mail.ru>
2022-09-20 14:11:28 +0300

    changed description NotFound Block

--- src/components/NotFoundBlock/index.tsx
+++ src/components/NotFoundBlock/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import styles from './NotFoundBlock.module.scss';
 
-const  NotFoundBlock: React.FC  = () => {
+const NotFoundBlock: React.FC = () => {
   return (
     <div className={styles.root}>
       <h1>
@@ -11,10 +11,10 @@ const  NotFoundBlock: React.FC  = () => {
         Ничего не найдено
       </h1>
       <p className={styles.description}>
-        К сожалению данная страница отсутствует в нашем интернет-магазине
+        К сожалению, данная страница отсутствует в нашем интернет-магазине
       </p>
     </div>
   );
-}
+};
 
 export default NotFoundBlock;
